a6f72827324327d759dfe695879385c398410c79
Daniel <daniel.martins@al.infnet.edu.br>
2020-11-11 11:32:15 -0300

    rota cadastro fixed

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('../../models/user');
+const User = require('../models/user');
 const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
